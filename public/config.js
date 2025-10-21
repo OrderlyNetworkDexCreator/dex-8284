ffdbfa15cb35e878a1d1b8158bce3426c85f132c
Mario Reder <mario.reder@pm.me>
2025-10-21 14:59:44 +0200

    Setup DEX with workflow files and configuration

--- public/config.js
+++ public/config.js
@@ -1,5 +1,5 @@
 window.__RUNTIME_CONFIG__ = {
-  "VITE_ORDERLY_BROKER_ID": "demo",
+  "VITE_ORDERLY_BROKER_ID": "coin-wallet-dex",
   "VITE_ORDERLY_BROKER_NAME": "Coin Wallet DEX",
   "VITE_DISABLE_MAINNET": "false",
   "VITE_DISABLE_TESTNET": "true",
